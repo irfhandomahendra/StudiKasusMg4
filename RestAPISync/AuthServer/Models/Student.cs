fbbf3932d8548585cdf29ec252720f9dd777c5ef
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-09 11:53:58 +0700

    tambah model untuk code first

---
+++ RestAPISync/AuthServer/Models/Student.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Fullname { get; set; }
+        
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
