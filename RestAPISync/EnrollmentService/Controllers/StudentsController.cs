d243818be2d25c8b7fc3dd96e68c57002ccfefc0
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-10 10:36:27 +0700

    tambah students controller

---
+++ RestAPISync/EnrollmentService/Controllers/StudentsController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using EnrollmentService.Data;
+using EnrollmentService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnrollmentService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private IStudent _student;
+        private IMapper _mapper;
+
+        public StudentsController(IStudent student, IMapper mapper)
+        {
+            _student = student ?? throw new ArgumentNullException(nameof(student));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentDto>>> Get()
+        {
+            var students = await _student.GetAll();
+            var dtos = _mapper.Map<IEnumerable<StudentDto>>(students);    
+            return Ok(dtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentDto>> Get(int id)
+        {
+            var result = await _student.GetById(id.ToString());
+            if (result == null)
+                return NotFound();
+            return Ok(_mapper.Map<StudentDto>(result));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<StudentDto>> Post([FromBody] StudentCreateDto studentCreateDto)
+        {
+            try
+            {
+                var student = _mapper.Map<Models.Student>(studentCreateDto);
+                var result = await _student.Insert(student);
+                var studentReturn = _mapper.Map<Dtos.StudentDto>(result);
+                return Ok(studentReturn);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<StudentDto>> Put(int id, [FromBody]StudentCreateDto studentCreateDto)
+        {
+            try
+            {
+                var student = _mapper.Map<Models.Student>(studentCreateDto);
+                var result = await _student.Update(id.ToString(), student);
+                var studentdto = _mapper.Map<Dtos.StudentDto>(result);
+                return Ok(studentdto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _student.Delete(id.ToString());
+                return Ok($"Data student {id} berhasil di delete");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- RestAPISync/EnrollmentService/Data/StudentDAL.cs
+++ RestAPISync/EnrollmentService/Data/StudentDAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EnrollmentService.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EnrollmentService.Data
 {
@@ -14,29 +15,66 @@ namespace EnrollmentService.Data
         {
             _db = db;
         }
-        public Task Delete(string id)
+        public async Task Delete(string id)
         {
-            throw new NotImplementedException();
+            var result = await GetById(id);
+            if (result == null) throw new Exception("Data tidak ditemukan !");
+            try
+            {
+                _db.Students.Remove(result);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
         }
 
-        public Task<IEnumerable<Student>> GetAll()
+        public async Task<IEnumerable<Student>> GetAll()
         {
-            throw new NotImplementedException();
+            var results = await _db.Students.OrderBy(s => s.Fullname).ToListAsync();
+            return results;
         }
 
-        public Task<Student> GetById(string id)
+        public async Task<Student> GetById(string id)
         {
-            throw new NotImplementedException();
+            var result = await _db.Students.Where(s => 
+            s.Id == Convert.ToInt32(id)).SingleOrDefaultAsync<Student>();
+            if (result != null)
+                return result;
+            else
+                throw new Exception("Data tidak ditemukan !");
         }
 
-        public Task<Student> Insert(Student obj)
+        public async Task<Student> Insert(Student obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _db.Students.Add(obj);
+                await _db.SaveChangesAsync();
+                return obj;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
         }
 
-        public Task<Student> Update(string id, Student obj)
+        public async Task<Student> Update(string id, Student obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await GetById(id);
+                result.Fullname = obj.Fullname;
+                result.EnrollmentDate = obj.EnrollmentDate;
+                await _db.SaveChangesAsync();
+                obj.Id = Convert.ToInt32(id);
+                return obj;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
         }
     }
 }
