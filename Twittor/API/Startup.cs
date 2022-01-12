193b026285c0b68ce3abf51e3e2c3f6fedb5bafa
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-12 12:30:22 +0700

    tambah setting jwt

--- Twittor/API/Startup.cs
+++ Twittor/API/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.GraphQL;
 using API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -31,8 +34,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-           var conString = Configuration.GetConnectionString("MyDatabase");
-            
+            var conString = Configuration.GetConnectionString("MyDatabase");
+
             services.AddDbContext<AppDbContext>(options =>
                  options.UseSqlServer(conString)
             );
@@ -49,8 +52,28 @@ namespace API
             services
                 .AddGraphQLServer()
                 .AddQueryType<Query>()
-                .AddMutationType<Mutation>();
-        
+                .AddMutationType<Mutation>()
+                .AddAuthorization();
+
+            services.AddControllers();
+            // DI Dependency Injection
+            services.Configure<TokenSettings>(Configuration.GetSection("TokenSettings"));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidIssuer = Configuration.GetSection("TokenSettings").GetValue<string>("Issuer"),
+                       ValidateIssuer = true,
+                       ValidAudience = Configuration.GetSection("TokenSettings").GetValue<string>("Audience"),
+                       ValidateAudience = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("TokenSettings").GetValue<string>("Key"))),
+                       ValidateIssuerSigningKey = true
+                   };
+
+               });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +90,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
