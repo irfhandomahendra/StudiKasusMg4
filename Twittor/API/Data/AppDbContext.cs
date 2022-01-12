949bd37862d81f4682e23ef41df03b5c56308724
irfhandomahendra <irfhandomahendra@gmail.com>
2022-01-12 12:18:35 +0700

    twittor: first init

---
+++ Twittor/API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Twittor> Twittors { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
